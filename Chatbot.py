f91e57c6ff706b5751fb77c0aa5f76e71697f208
Emile Schjeldsøe Berg <emile.schjeldsoe.berg@frende.no>
2023-09-28 14:09:48 +0200

    fix?

--- Chatbot.py
+++ Chatbot.py
@@ -73,14 +73,14 @@ st.caption("Hr Chat")
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
-for msg in st.session_state.messages:
+for msg in st.session_state["messages"]:
     st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input():
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state["messages"].append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
     
     response = get_answer(st.session_state.messages)
     msg = response["choices"][0]["message"]["content"]
-    st.session_state.messages.append(msg)
+    st.session_state["messages"].append(msg)
     st.chat_message("assistant").write(msg.content)
